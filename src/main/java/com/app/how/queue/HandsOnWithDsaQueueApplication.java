2418a360f88474c32ce89ca473e0417c9ce1af12
HPxIO <hpx.io@keemail.me>
2021-12-01 15:41:53 +0530

    initial commit

---
+++ src/main/java/com/app/how/queue/HandsOnWithDsaQueueApplication.java
@@ -0,0 +1,13 @@
+package com.app.how.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnWithDsaQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnWithDsaQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/how/queue/HandsOnWithDsaQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.how.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnWithDsaQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
